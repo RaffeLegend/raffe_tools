26d709c7028fe6cafc9e0e5e97ca8ab65d2bf7cc
RaffeLegend <oldgirafferiver@gmail.com>
2024-12-08 18:04:38 +0000

    add num support

--- image_frequency.py
+++ image_frequency.py
@@ -39,7 +39,7 @@ def fft_filter(im, mode=1):
         im[:,:,i] = fft_img
     return np.transpose(im, (2,0,1))
 
-def get_average_frequency(directory, method='FFT'):
+def get_average_frequency(directory, method='FFT', nums_images=200):
     image_list = get_image_list(directory)
     frequency_sum = None
     count = 0
@@ -54,8 +54,6 @@ def get_average_frequency(directory, method='FFT'):
             f_transform = cv2.dft(np.float32(image), flags=cv2.DFT_COMPLEX_OUTPUT)
             magnitude_spectrum = 20 * np.log(cv2.magnitude(f_transform[:, :, 0], f_transform[:, :, 1]) + 1)
         elif method == 'FFT':
-            # f_transform = np.fft.fft2(image)
-            # f_shift = np.fft.fftshift(f_transform)
             f_transform = fftpack.fft2(image)
             f_shift = fftpack.fftshift(f_transform)
             magnitude_spectrum = 20 * np.log(np.abs(f_shift) + 1)
@@ -77,6 +75,8 @@ def get_average_frequency(directory, method='FFT'):
         
         frequency_sum += resized_magnitude_spectrum
         count += 1
+        if count >= nums_images:
+            break
     progress_bar.close()
     
     if count == 0:
@@ -94,14 +94,6 @@ def save_average_frequency_image(directory, output_path, method='FFT'):
     
     # Convert to color image
     color_average_frequency = cv2.applyColorMap(normalized_frequency, cv2.COLORMAP_JET)
-    
-    # Enhance contrast using CLAHE (Contrast Limited Adaptive Histogram Equalization)
-    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    # lab = cv2.cvtColor(color_average_frequency, cv2.COLOR_BGR2LAB)
-    # lab_planes = list(cv2.split(lab))
-    # lab_planes[0] = clahe.apply(lab_planes[0])
-    # lab = cv2.merge(lab_planes)
-    # color_average_frequency = cv2.cvtColor(lab, cv2.COLOR_LAB2BGR)
     
     # Save the result image
     output_name = 'average_frequency.png'
@@ -124,55 +116,9 @@ def get_image_list(directory):
 
     return image_files
 
-def get_frequency(image_path, output_path, method='DCT'):
-    # Step 1: 加载图像
-    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-
-    # Step 2: 计算傅里叶变换
-    if method == 'DFT':
-        f_transform = cv2.dft(np.float32(image), flags=cv2.DFT_COMPLEX_OUTPUT)
-        magnitude_spectrum = 20 * np.log(cv2.magnitude(f_transform[:, :, 0], f_transform[:, :, 1]) + 1)
-    elif method == 'FFT':
-        f_transform = np.fft.fft2(image)
-        f_shift = np.fft.fftshift(f_transform)  # 移频操作，低频移动到中心
-        magnitude_spectrum = 1 * np.log(np.abs(f_shift) + 1)  # 计算幅值谱
-    elif method == 'DCT':
-        f_transform = cv2.dct(np.float32(image))
-        magnitude_spectrum = np.log(np.abs(f_transform) + 1)
-    else:
-        raise ValueError("Unsupported method. Choose from 'DFT', 'FFT', or 'DCT'.")
-    # Step 3: 将幅值谱归一化到0-255并转换为8位图像
-    magnitude_spectrum = np.uint8(255 * (magnitude_spectrum - np.min(magnitude_spectrum)) / (np.max(magnitude_spectrum) - np.min(magnitude_spectrum)))
-    
-    # Step 4: 将灰度图像转换为彩色图像
-    color_magnitude_spectrum = cv2.applyColorMap(magnitude_spectrum, cv2.COLORMAP_JET)
-    
-    # Step 4.1: 使用拉普拉斯算子增强边缘
-    # laplacian = cv2.Laplacian(image, cv2.CV_64F)
-    # laplacian = np.uint8(255 * (laplacian - np.min(laplacian)) / (np.max(laplacian) - np.min(laplacian)))
-    # color_laplacian = cv2.applyColorMap(laplacian, cv2.COLORMAP_JET)
-
-    # Step 4.2: 将幅值谱和边缘图像进行融合
-    # combined_image = cv2.addWeighted(color_magnitude_spectrum, 0.7, color_laplacian, 0.3, 0)
-    # Step 5: 保存结果图像
-    output_name = os.path.splitext(os.path.basename(image_path))[0] + '_frequency.png'
-    output_path = os.path.join(output_path, output_name)
-    cv2.imwrite(output_path, color_magnitude_spectrum)
-    return combined_image
-
 if __name__ == "__main__":
     input_path = '/mnt/data2/users/hilight/datasets/FakeSocialMedia/Images/sdxl_lightning'
     # output_path = '/mnt/data2/users/hilight/yiwei/dataset/frequency'
     output_path = './'
     os.makedirs(output_path, exist_ok=True)
     save_average_frequency_image(input_path, output_path, method='FFT')
-    # image_list = get_image_list(directory=input_path)
-    # for image_path in image_list:
-    #     # Create the corresponding directory structure in the output path
-    #     relative_path = os.path.relpath(image_path, input_path)
-    #     output_image_path = os.path.join(output_path, os.path.dirname(relative_path))
-    #     os.makedirs(output_image_path, exist_ok=True)
-        
-    #     # Generate and save the frequency image
-    #     get_frequency(image_path, output_image_path)
-    #     print(f"Frequency image saved for {image_path}")
